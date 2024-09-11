0502585e49710f111841e297aa30d8d3b98489ea
psychotalbain <psychotalbain@gmail.com>
2024-09-11 11:15:00 -0300

    feat: configuration babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset']
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@context': './src/context',
+          '@data': './src/data',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@types': './src/types',
+          '@utils': './src/utils',
+          '@views': './src/views'
+        }
+      }
+    ]
+  ]
 }
